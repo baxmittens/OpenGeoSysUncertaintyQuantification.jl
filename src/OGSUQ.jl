d7466e5d478cab704bd6f538c5418388364261ab
Bittens <Bittens.M@bgr.local>
2023-03-06 13:00:21 +0100

    ins

--- src/OGSUQ.jl
+++ src/OGSUQ.jl
@@ -115,6 +115,42 @@ function start!(ogsuqasg::OGSUQASG)
 	end
 end
 
+function exp_val_func(x,ID,ogsuqasg::OGSUQASG,retval_proto)
+	ret = similar(retval_proto)
+	interpolate!(ret,ogsuqasg.asg, x)
+	mul!(ret,pdf(ogsuqasg.stochasticmodelparams.stochparams, x))
+	return ret
+end
+
+function 𝔼(ogsuqasg::OGSUQASG)
+	retval_proto = deepcopy(first(ogsuqasg.asg).scaling_weight)
+	_exp_val_func(x,ID) = exp_val_func(x,ID,ogsuqasg,retval_proto)
+	asg = ASG(ogsuqasg, _exp_val_func, ogsuqasg.ogsuqparams.samplemethodparams.tol)
+	return integrate_inplace_ops(asg)
+end
+
+function 𝔼(sogs) 
+	return 𝔼(sogs.analysis,sogs) 
+end
+
+function var_func(x,ID,ogsuqasg::OGSUQASG, exp_val::RT)
+	stochparams = ogsuqasg.stochasticmodelparams.stochparams
+	asg = ogsuqasg.asg
+	ret = similar(exp_val)
+	interpolate!(ret,asg,x)
+	minus!(ret,exp_val)
+	pow!(ret,2.0)
+	mul!(ret,pdf(stochparams, x))
+	#return ((ret-exp_val).^2)*pdf(stochparams, x)
+	return ret
+end
+
+function var(ogsuqasg::OGSUQASG,exp_val::RT)
+	_var_func(x,ID) = var_func(x,ID,ogsuqasg,exp_val)
+	asg = ASG(ogsuqasg, _var_func, ogsuqasg.ogsuqparams.samplemethodparams.tol)
+	return integrate_inplace_ops(asg)
+end
+
 include("./OGSUQ/utils.jl")
 
 export OGS6ProjectParams, StochasticOGS6Parameter, StochasticOGSModelParams, SampleMethodParams, SparseGridParams, 
