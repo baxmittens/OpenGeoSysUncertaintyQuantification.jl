22848981af236a927765f8447742e346642d5cb2
Bittens <Bittens.M@bgr.local>
2024-03-08 14:47:49 +0100

    ins

--- src/OpenGeoSysUncertaintyQuantification/utils.jl
+++ src/OpenGeoSysUncertaintyQuantification/utils.jl
@@ -236,34 +236,12 @@ function gethyperedges(asg::DistributedSparseGrids.AdaptiveHierarchicalSparseGri
 	return cpts
 end
 
-AltInplaceOpsInterface.add!(a::Vector{Float64}, b::Vector{Float64}) = a .+= b
-mul!(a::Vector{Float64}, b::Float64) = a .*= b
-mul!(a::Vector{Float64}, b::Vector{Float64}) = a .*= b
-mul!(a::Vector{Float64}, b::Vector{Float64}, c::Float64) = begin; for i = 1:length(a); a[i] = b[i]*c; end; return nothing; end
-AltInplaceOpsInterface.minus!(a::Vector{Float64}, b::Vector{Float64}) = a .-= b
-AltInplaceOpsInterface.pow!(a::Vector{Float64}, b::Int64) = a .^= b
-AltInplaceOpsInterface.pow!(a::Vector{Float64}, b::Float64) = a .^= b
+function integrate_nodal_result(field,xdmf::T,modeldef::Ogs6ModelDef) where T
+	error("integrate_nodal_result not implemented for type $T")
+end
 
-Base.fill!(a::Vector{Vector{Float64}}, b::Float64) = foreach(x->fill!(x,b), a)
-Base.similar(a::Vector{Vector{Float64}}) = map(x->similar(x),a)
-
-AltInplaceOpsInterface.add!(a::Vector{Vector{Float64}}, b::Vector{Vector{Float64}}) = a .+= b
-mul!(a::Vector{Vector{Float64}}, b::Float64) = a .*= b
-mul!(a::Vector{Vector{Float64}}, b::Vector{Vector{Float64}}) = begin; for i = 1:length(a); mul!(a[i],b[i]) end; return nothing; end
-mul!(a::Vector{Vector{Float64}}, b::Vector{Vector{Float64}}, c::Float64) = begin; for i = 1:length(a); a[i] = b[i].*c; end; return nothing; end
-AltInplaceOpsInterface.minus!(a::Vector{Vector{Float64}}, b::Vector{Vector{Float64}}) = a .-= b
-AltInplaceOpsInterface.pow!(a::Vector{Vector{Float64}}, b::Int64) = a .^= b
-AltInplaceOpsInterface.pow!(a::Vector{Vector{Float64}}, b::Float64) = begin; for i = 1:length(a); pow!(a[i],b) end; return nothing; end
-
-AltInplaceOpsInterface.add!(a::Matrix{Float64}, b::Matrix{Float64}) = a .+= b
-LinearAlgebra.mul!(a::Matrix{Float64}, b::Float64) = a .*= b
-LinearAlgebra.mul!(a::Matrix{Float64}, b::Matrix{Float64}, c::Float64) = begin; for i = 1:length(a); a[i] = b[i]*c; end; return nothing; end
-AltInplaceOpsInterface.minus!(a::Matrix{Float64}, b::Matrix{Float64}) = a .-= b
-AltInplaceOpsInterface.pow!(a::Matrix{Float64}, b::Int64) = a .^= b
-AltInplaceOpsInterface.pow!(a::Matrix{Float64}, b::Float64) = a .^= b
-
-function integrate_result(field,xdmf::T,modeldef::Ogs6ModelDef) where T
-	error("integrate_result not implemented for type $T")
+function integrate_cell_result(field,xdmf::T,modeldef::Ogs6ModelDef) where T
+	error("integrate_cell_result not implemented for type $T")
 end
 
 function integrate_nodal_result(field, xdmf::XDMF3File, modeldef::Ogs6ModelDef)
@@ -318,35 +296,3 @@ function integrate_area(field, xdmf::XDMF3File, modeldef::Ogs6ModelDef)
 	return valA
 end
 
-ogs6_modeldef(ogsparams::OGS6ProjectParams) = read(Ogs6ModelDef, ogsparams.projectfile)
-ogs6_modeldef(stochparams::StochasticOGSModelParams) = ogs6_modeldef(stochparams.ogsparams)
-ogs6_modeldef(ogsuqparams::OGSUQParams) = ogs6_modeldef(ogsuqparams.stochasticmodelparams)
-ogs6_modeldef(ogsuq::AbstractOGSUQ) = ogs6_modeldef(ogsuq.ogsuqparams)
-
-ogs6_simcall(ogsparams::OGS6ProjectParams) = ogsparams.simcall
-ogs6_simcall(stochparams::StochasticOGSModelParams) = ogs6_simcall(stochparams.ogsparams)
-ogs6_simcall(ogsuqparams::OGSUQParams) = ogs6_simcall(ogsuqparams.stochasticmodelparams)
-ogs6_simcall(ogsuq::AbstractOGSUQ) = ogs6_simcall(ogsuq.ogsuqparams)
-
-ogs6_outputpath(ogsparams::OGS6ProjectParams) = ogsparams.outputpath
-ogs6_outputpath(stochparams::StochasticOGSModelParams) = ogs6_outputpath(stochparams.ogsparams)
-ogs6_outputpath(ogsuqparams::OGSUQParams) = ogs6_outputpath(ogsuqparams.stochasticmodelparams)
-ogs6_outputpath(ogsuq::AbstractOGSUQ) = ogs6_outputpath(ogsuq.ogsuqparams)
-
-ogs6_additionalprojecfilespath(ogsparams::OGS6ProjectParams) = ogsparams.additionalprojecfilespath
-ogs6_additionalprojecfilespath(stochparams::StochasticOGSModelParams) = ogs6_additionalprojecfilespath(stochparams.ogsparams)
-ogs6_additionalprojecfilespath(ogsuqparams::OGSUQParams) = ogs6_additionalprojecfilespath(ogsuqparams.stochasticmodelparams)
-ogs6_additionalprojecfilespath(ogsuq::AbstractOGSUQ) = ogs6_additionalprojecfilespath(ogsuq.ogsuqparams)
-
-ogs6_postprocfiles(stogsparamsochparams::OGS6ProjectParams) = ogsparams.postprocfiles
-ogs6_postprocfiles(stochparams::StochasticOGSModelParams) = ogs6_postprocfiles(stochparams.ogsparams)
-ogs6_postprocfiles(ogsuqparams::OGSUQParams) = ogs6_postprocfiles(ogsuqparams.stochasticmodelparams)
-ogs6_postprocfiles(ogsuq::AbstractOGSUQ) = ogs6_postprocfiles(ogsuq.ogsuqparams)
-
-stoch_parameters(stochasticmodelparams::StochasticOGSModelParams) = stochasticmodelparams.stochparams
-stoch_parameters(ogsuqparams::OGSUQParams) = stoch_parameters(ogsuqparams.stochasticmodelparams)
-stoch_parameters(ogsuq::AbstractOGSUQ) = stoch_parameters(ogsuq.ogsuqparams)
-
-stoch_samplemethod(stochasticmodelparams::StochasticOGSModelParams) = stochasticmodelparams.samplemethod
-stoch_samplemethod(ogsuqparams::OGSUQParams) = stoch_samplemethod(ogsuqparams.stochasticmodelparams)
-stoch_samplemethod(ogsuq::AbstractOGSUQ) = stoch_samplemethod(ogsuq.ogsuqparams)
