add2dacced3904987afce510c9cc5b2250800a2d
Bittens <Bittens.M@bgr.local>
2024-03-10 19:41:24 +0100

    ins

--- src/OpenGeoSysUncertaintyQuantification/utils_print.jl
+++ src/OpenGeoSysUncertaintyQuantification/utils_print.jl
@@ -1,7 +1,8 @@
-import Base.display
+import Base.show
 
-function display(io::IO, stochparams::Vector{StochasticOGS6Parameter})
+function show(io::IO, mime::MIME"text/plain", stochparams::AbstractArray{StochasticOGS6Parameter})
 	N = length(stochparams)
+	title = "StochasticOGS6Parameters"
 	header = ["#","Name", "Dist.", "lower_bound", "upper_bound", "valspec"]
 	data = Matrix{Any}(undef, N, 6)
 	for (i,stochparam) in enumerate(stochparams)
@@ -12,5 +13,70 @@ function display(io::IO, stochparams::Vector{StochasticOGS6Parameter})
 		data[i,5] = stochparam.upper_bound
 		data[i,6] = stochparam.valspec
 	end
-	return pretty_table(data; header)
+	println(io)
+	pretty_table(io, data; header=header, title=title)
+end
+
+function show(io::IO, ogs6proparams::OGS6ProjectParams)
+	title = "OGS6ProjectParams"
+	header = ["Paramemter","Value"]
+	data = [
+		"Project file" ogs6proparams.projectfile;
+		"Simcall" ogs6proparams.simcall;
+		"Add. files path" ogs6proparams.additionalprojecfilespath;
+		"Output path" ogs6proparams.outputpath
+		]
+	postproc = [["Postproc file $i", ogs6proparams.postprocfiles[i]] for i = 1:length(ogs6proparams.postprocfiles)]
+	println(io)
+	pretty_table(io, vcat(data,permutedims(hcat(postproc...))); header=header, title=title)
+end
+
+function show(io::IO, ogsmodelparams::StochasticOGSModelParams)
+	title = "StochasticOGSModelParams"
+	header = ["Paramemter","Value"]
+	data = [
+		"Samplemethod" ogsmodelparams.samplemethod;
+		"# local workers" ogsmodelparams.num_local_workers;
+		"Userfunction file" ogsmodelparams.userfunctionfile;
+		"file" ogsmodelparams.file
+		]
+	println(io)
+	pretty_table(io, data; header=header, title=title)
+	show(io, ogsmodelparams.ogsparams)
+	show(io, "text/plain", ogsmodelparams.stochparams)
+end
+
+function show(io::IO, smp::SMP) where SMP<:SampleMethodParams
+	_fieldnames = fieldnames(SMP)
+	title = "$SMP"
+	header = ["Paramemter","Value"]
+	postproc = [[_fieldname, getfield(smp, _fieldname)] for _fieldname in _fieldnames]
+	println(io)
+	pretty_table(io, permutedims(hcat(postproc...)); header=header, title=title)
+end
+
+function show(io::IO, ogsuqparams::OGSUQParams)
+	println(io)
+	printstyled(io, "OGSUQParams"; color = :light_white)
+	println(io)
+	show(io, ogsuqparams.stochasticmodelparams)
+	show(io, ogsuqparams.samplemethodparams)
+end
+
+function show(io::IO, ogsuq::OGSUQASG)
+	println(io)
+	printstyled(io, "OGSUQASG"; color = :light_white)
+	println(io)
+	show(io, ogsuq.ogsuqparams)
+	println(io)
+	show(io, ogsuq.asg)
+end
+
+function show(io::IO, ogsuq::T) where T<:AbstractOGSUQMonteCarlo
+	println(io)
+	printstyled(io, "$T"; color = :light_white)
+	println(io)
+	show(io, ogsuq.ogsuqparams)
+	println(io)
+	show(io, ogsuq.mc)
 end
