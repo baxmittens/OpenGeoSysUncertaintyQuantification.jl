8cfaa6068c315f49f517827b3bae7b4cce738bf4
Bittens <Bittens.M@bgr.local>
2024-03-13 13:31:35 +0100

    ins

--- test/test_project_setup.jl
+++ test/test_project_setup.jl
@@ -2,3 +2,5 @@ file_stoch_model = "./ex1/altered_StochasticOGSModelParams.xml"
 file_sample_params = "./ex1/altered_SampleMethodParams.xml"
 
 ogsuqparams = OGSUQParams(file_stoch_model, file_sample_params)
+
+@test ogsuqparams.stochasticmodelparams.samplemethod == AdaptiveHierarchicalSparseGrid
