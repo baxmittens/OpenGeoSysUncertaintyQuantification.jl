ac36d5e3b61586e234c07be2c8c153ce58c23aac
Bittens <Bittens.M@bgr.local>
2024-05-30 12:05:42 +0200

    update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,7 @@ using Test
 end
 
 @testset "Stochastic model setup" begin
-    include("ex1/generate_stoch_params_file.jl")
+    include("ex1/generate_stoch_mode_lowres.jl")
 end
 
 @testset "Utils" begin
